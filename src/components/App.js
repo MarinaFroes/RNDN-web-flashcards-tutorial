3c6588fbc9c28fd39a7d57d9065ce3090f70e721
MFAC <ninafroes@gmail.com>
2020-02-17 14:51:50 +0100

    feat: Import DeckList component

--- src/components/App.js
+++ src/components/App.js
@@ -1,68 +1,28 @@
 import React, { Component }  from 'react'
 import '../App.css'
-import { getDecks, saveDeckTitle } from '../utils/api'
+import { handleReceiveDecks } from '../redux/actions/decks'
+
+import DeckList from './DeckList'
+import { connect } from 'react-redux'
 
 class App extends Component {
-  state = {
-    question: '',
-    answer: '',
-    deckTitle: '',
-    decks: {}
-  }
 
   componentDidMount() {
-    getDecks().then(decks => {
-      this.setState({ decks })
-    })
+    const { dispatch } = this.props
+
+    dispatch(handleReceiveDecks())
   }
   
-  handleChange = (e) => {
-    this.setState({
-      deckTitle: e.target.value
-    })
-  }
-
-  handleSubmit = e => {
-    e.preventDefault()
-
-    saveDeckTitle(this.state.deckTitle)
-      .then(() => {
-        this.setState({
-          deckTitle: ''
-        })
-    })
-  }
 
   render() {
-    console.log("DECKS:")
-    console.log(this.state.decks)
     
     return (
       <div>
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Add deck title:
-            <input
-              type="text"
-              name="deckTitle"
-              id="deckTitle"
-              value={this.state.deckTitle}
-              placeholder="Title"
-              onChange={this.handleChange}
-              required
-            />
-          </label>
-          <button type="submit">Save</button>
-        </form>
-        {
-          Object.keys(this.state.decks)
-            .map(deck => (
-              <p>{deck.title}</p>
-            ))
-        }
+        <DeckList />
       </div>
     )
   }
 }
 
-export default App;
+export default connect()(App);
+
