5e4bff29cef2dac50c61eb90054e37b5f0e451e4
MFAC <ninafroes@gmail.com>
2020-02-17 21:57:10 +0100

    feat: Add Link to deck

--- src/components/DeckPreview.js
+++ src/components/DeckPreview.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function DeckPreview({ decks, deck_id }) {
   return (
-    <div style={{ border: '2px solid red' }}>
-      <p>{decks[deck_id].title}</p>
-      <p>{decks[deck_id].questions ? decks[deck_id].questions.length : 0} cards</p>
-    </div>
+    <Link to={`/deck/${deck_id}`} >
+      <div style={{ border: '2px solid red' }}>
+        <p>{decks[deck_id].title}</p>
+        <p>{decks[deck_id].questions ? decks[deck_id].questions.length : 0} cards</p>
+      </div>
+    </Link>
   )
 }
 
