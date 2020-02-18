4cab69f9195efaaa42789a7ff84eff5bf450c8c4
MFAC <ninafroes@gmail.com>
2020-02-18 09:18:22 +0100

    feat: Change addCard to return deck instead of just the question and answer

--- src/redux/actions/cards.js
+++ src/redux/actions/cards.js
@@ -4,24 +4,26 @@ import { addCardToDeck } from '../../utils/api'
 export const ADD_CARD = 'ADD_CARD'
 // export const DELETE_CARD = 'DELETE_CARD'
 
-export function addCard({ deck_id, question, answer }) {
+export function addCard(deck) {
+  console.log('---ADD CARD---')
+  console.log(deck)
+  
   return {
     type: ADD_CARD,
-    deck_id,
-    question,
-    answer
+    deck
   }
 }
 
 export function handleAddCard(card) {
   return dispatch => {
     dispatch(showLoading())
-
+    console.log('----HANDLE ADD CARD----')
+    console.log(card)
     return addCardToDeck(card)
       .then(res => {
         dispatch(addCard(res))
         dispatch(hideLoading())
-      })
+      }).catch(err => console.log(err))
   }
 }
 
